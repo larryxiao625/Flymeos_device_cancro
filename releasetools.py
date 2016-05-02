854770e82ff2f1f409ed7fe050fa17eb24da56bb
nian0114 <268078545@qq.com>
2016-05-02 18:11:07 +0800

    remove script assest

---
+++ releasetools.py
@@ -0,0 +1,36 @@
+import common
+import edify_generator
+import os
+
+def RemoveDeviceAssert(info):
+  edify = info.script
+  for i in xrange(len(edify.script)):
+    if "assert" in edify.script[i]:
+      edify.script[i] = ''
+      return
+
+def RemoveDeviceGetprop(info):
+  edify = info.script
+  for i in xrange(len(edify.script)):
+    if "getprop" in edify.script[i]:
+      edify.script[i] = ''
+      return
+
+def WritePolicyConfig(info):
+  try:
+    file_contexts = info.input_zip.read("META/file_contexts")
+    common.ZipWriteStr(info.output_zip, "file_contexts", file_contexts)
+  except KeyError:
+    print "warning: file_context missing from target;"
+
+def InstallImage(img_name, img_file, partition, info):
+  common.ZipWriteStr(info.output_zip, img_name, img_file)
+  info.script.AppendExtra(('package_extract_file("' + img_name + '", "' + partition + '");'))
+
+def FullOTA_InstallEnd(info):
+    RemoveDeviceAssert(info)
+    RemoveDeviceGetprop(info)
+
+def IncrementalOTA_InstallEnd(info):
+    RemoveDeviceAssert(info)
+    RemoveDeviceGetprop(info)
