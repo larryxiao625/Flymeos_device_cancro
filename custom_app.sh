49a5047aff49eb7a6bb14913852278451eb2de92
nian0114 <268078545@qq.com>
2016-05-07 04:43:17 +0800

    flyme upgrade

--- custom_app.sh
+++ custom_app.sh
@@ -1,43 +1,8 @@
-#/bin/bash
 
-curdir=`pwd`
-
-echo "target dir:$1"
-echo "original dir: $2"
-
-function applyPatch () {
-    for patch in `find $1 -name *.patch`
-    do
-        cd out
-        $GIT_APPLY ../$patch
-        cd ..
-        for rej in `find $2 -name *.rej`
-        do
-            echo "Patch $patch fail"
-            exit 1
-        done
-    done
-}
-
-function appendSmaliPart() {
-    for file in `find $1/smali -name *.part`
-    do
-		filepath=`dirname $file`
-		filename=`basename $file .part`
-		dstfile="out/$filepath/$filename"
-        cat $file >> $dstfile
-    done
-}
-
-function mergyXmlPart() {
-	for file in `find $1/res -name *.xml.part`
-	do
-		src=`dirname $file`
-		dst=${src/$1/$2}
-		$XMLMERGYTOOL $src $dst
-	done
-}
+apkBaseName=$1
+tempSmaliDir=$2
 
 if [ $1 = "TeleService" ];then
-	appendSmaliPart "TeleService"
+	echo ">>> in custom_jar $apkBaseName"
+	cat TeleService/smali/com/android/phone/PhoneInterfaceManager.smali.part >> $tempSmaliDir/smali/com/android/phone/PhoneInterfaceManager.smali
 fi
